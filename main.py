56cf4d66efade38a702addc1368f0f682c7ddc79
Jesse Kleemann <j.kleemann@tu-bs.de>
2021-08-30 19:35:53 +0200

    display values by scaling and description

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import json
 import sys
 import threading
 import datetime
@@ -7,10 +8,11 @@ from pathlib import Path
 from typing import Callable
 
 import can
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QTableWidgetItem
+from PyQt5.QtWidgets import QTableWidgetItem, QDialog, QMainWindow
 
+from ui.display import Ui_Dialog
 from ui.main import Ui_MainWindow
 
 
@@ -100,6 +102,140 @@ class CanLogger:
         self.can1_to_can0.running = False
 
 
+class DisplayDialog(Ui_Dialog):
+    def __init__(self, parent: QMainWindow, can_logger: CanLogger):
+        self.dialog = QDialog(parent, QtCore.Qt.WindowCloseButtonHint)
+        self.setupUi(self.dialog)
+
+        self.can_logger = can_logger
+
+        self.pushButtonClose.clicked.connect(self.close_and_save)
+        self.pushButtonInsert.clicked.connect(self.insert_row)
+        self.pushButtonAutosize.clicked.connect(self.autosize_table)
+        self.pushButtonDelete.clicked.connect(self.delete_row)
+
+        self.labels = ['ID Hex', 'Startbit', 'Endbit', 'Signed', 'Scaling', 'Description', 'Value']
+        self.tableWidgetDisplay.setColumnCount(len(self.labels))
+        self.tableWidgetDisplay.setHorizontalHeaderLabels(self.labels)
+
+        self.display_messages = []
+        self.load_config()
+
+        timer = QTimer(self.dialog)
+        timer.timeout.connect(self.check_values)
+        timer.start(1000)
+
+        self.dialog.exec_()
+
+    def delete_row(self):
+        self.tableWidgetDisplay.removeRow(self.tableWidgetDisplay.currentRow())
+
+    def load_config(self):
+        if Path('display.json').is_file():
+            with open('display.json', 'r') as f:
+                self.display_messages = json.load(f)
+            self.tableWidgetDisplay.clear()
+            self.tableWidgetDisplay.setHorizontalHeaderLabels(self.labels)
+            for i, display_message in enumerate(self.display_messages):
+                self.tableWidgetDisplay.insertRow(i)
+
+                self.tableWidgetDisplay.setItem(i, self.labels.index('ID Hex'),
+                                                QTableWidgetItem(display_message['can_id_hex']))
+                self.tableWidgetDisplay.setItem(i, self.labels.index('Startbit'),
+                                                QTableWidgetItem(display_message['startbit']))
+                self.tableWidgetDisplay.setItem(i, self.labels.index('Endbit'),
+                                                QTableWidgetItem(display_message['endbit']))
+                self.tableWidgetDisplay.setItem(i, self.labels.index('Signed'),
+                                                QTableWidgetItem(display_message['signed']))
+                self.tableWidgetDisplay.setItem(i, self.labels.index('Scaling'),
+                                                QTableWidgetItem(display_message['scaling']))
+                self.tableWidgetDisplay.setItem(i, self.labels.index('Description'),
+                                                QTableWidgetItem(display_message['description']))
+
+    def close_and_save(self):
+        self.save_config()
+        self.dialog.close()
+
+    def save_config(self):
+        with open('display.json', 'w') as f:
+            json.dump(self.display_messages, f, sort_keys=True)
+
+    def insert_row(self):
+        self.tableWidgetDisplay.insertRow(self.tableWidgetDisplay.rowCount())
+
+    def check_values(self):
+        self.display_messages.clear()
+        for i in range(0, self.tableWidgetDisplay.rowCount()):
+            display_message = {}
+
+            can_id_hex_item = self.tableWidgetDisplay.item(i, self.labels.index('ID Hex'))
+            if can_id_hex_item is None:
+                continue
+            can_id_hex = can_id_hex_item.text()
+            display_message['can_id_hex'] = can_id_hex
+            try:
+                can_id = bytes.fromhex(can_id_hex)
+            except ValueError:
+                continue
+            can_id = int.from_bytes(can_id, byteorder='big', signed=False)
+
+            startbit = self.tableWidgetDisplay.item(i, self.labels.index('Startbit'))
+            if startbit is None:
+                continue
+            startbit = startbit.text()
+            display_message['startbit'] = startbit
+            if not startbit.isnumeric():
+                continue
+            startbit = int(startbit)
+
+            endbit = self.tableWidgetDisplay.item(i, self.labels.index('Endbit'))
+            if endbit is None:
+                continue
+            endbit = endbit.text()
+            display_message['endbit'] = endbit
+            if not endbit.isnumeric():
+                continue
+            endbit = int(endbit)
+
+            signed = self.tableWidgetDisplay.item(i, self.labels.index('Signed'))
+            if signed is None:
+                continue
+            signed = signed.text()
+            display_message['signed'] = signed
+            if not signed.isnumeric():
+                continue
+            signed = bool(signed)
+
+            scaling = self.tableWidgetDisplay.item(i, self.labels.index('Scaling'))
+            if scaling is None:
+                continue
+            scaling = scaling.text()
+            display_message['scaling'] = scaling
+            try:
+                scaling = float(scaling)
+            except ValueError:
+                continue
+
+            description = self.tableWidgetDisplay.item(i, self.labels.index('Description'))
+            if description is None:
+                description = ''
+            else:
+                description = description.text()
+            display_message['description'] = description
+
+            self.display_messages.append(display_message)
+
+            with self.can_logger.dict_lock:
+                if can_id in self.can_logger.latest_messages:
+                    message = self.can_logger.latest_messages[can_id]
+                    value = int.from_bytes(message.data[startbit:endbit], byteorder="big", signed=signed)
+                    value = scaling * value
+                    self.tableWidgetDisplay.setItem(i, self.labels.index('Value'), QTableWidgetItem(f'{value:.2f}'))
+
+    def autosize_table(self):
+        self.tableWidgetDisplay.resizeColumnsToContents()
+
+
 class MainWindow(Ui_MainWindow):
 
     def __init__(self):
@@ -111,11 +247,14 @@ class MainWindow(Ui_MainWindow):
         self.pushButtonCan0ToCan1.clicked.connect(self.can_logger.can0_to_can1.start_stop_thread)
         self.pushButtonCan1ToCan0.clicked.connect(self.can_logger.can1_to_can0.start_stop_thread)
         self.pushButtonAutosize.clicked.connect(self.autosize_table)
+        self.pushButtonDisplay.clicked.connect(self.display_values)
 
-        # self.can_logger.load_log('logs/can1_to_can0.txt')
+        self.can_logger.load_log('logs/can1_to_can0.txt')
         # self.can_logger.load_log('logs/can0_to_can1.txt')
         # self.can_logger.load_log('logs/logfile.txt')
 
+        self.labels = ['Timestamp', 'Time', 'ID Hex', 'ID Dec', 'Data', '0U16', '0S16', '1U16', '1S16', '2U16', '2S16',
+                       '3U16', '3S16', '0U32', '0S32', '1U32', '1S32', 'Interval', 'Channel']
         self.refresh_values()
         self.tableWidgetMessages.resizeColumnsToContents()
 
@@ -129,6 +268,9 @@ class MainWindow(Ui_MainWindow):
     def show(self):
         self.main_window.show()
         self.app.exec_()
+
+    def display_values(self):
+        DisplayDialog(self.main_window, self.can_logger)
 
     def autosize_table(self):
         self.tableWidgetMessages.resizeColumnsToContents()
@@ -145,28 +287,27 @@ class MainWindow(Ui_MainWindow):
         with self.can_logger.dict_lock:
             self.tableWidgetMessages.clear()
             self.tableWidgetMessages.setRowCount(len(self.can_logger.latest_messages))
-            labels = ['Timestamp', 'Time', 'ID Hex', 'ID Dec', 'Data', '0U16', '0S16', '1U16', '1S16', '2U16', '2S16',
-                      '3U16', '3S16', '0U32', '0S32', '1U32', '1S32', 'Interval', 'Channel']
-            self.tableWidgetMessages.setColumnCount(len(labels))
-            self.tableWidgetMessages.setHorizontalHeaderLabels(labels)
+            self.tableWidgetMessages.setColumnCount(len(self.labels))
+            self.tableWidgetMessages.setHorizontalHeaderLabels(self.labels)
             for i, latest_message in enumerate(sorted(self.can_logger.latest_messages)):
                 message = self.can_logger.latest_messages[latest_message]
-                self.tableWidgetMessages.setItem(i, labels.index('Timestamp'),
+                self.tableWidgetMessages.setItem(i, self.labels.index('Timestamp'),
                                                  QTableWidgetItem(f"{message.timestamp:>15.2f}"))
                 timestamp = f'{datetime.datetime.fromtimestamp(message.timestamp):%H:%M:%S}'
-                self.tableWidgetMessages.setItem(i, labels.index('Time'), QTableWidgetItem(timestamp))
+                self.tableWidgetMessages.setItem(i, self.labels.index('Time'), QTableWidgetItem(timestamp))
                 if message.is_extended_id:
                     arbitration_id_string = f"{message.arbitration_id:08x}"
                 else:
                     arbitration_id_string = f"{message.arbitration_id:04x}"
-                self.tableWidgetMessages.setItem(i, labels.index('ID Hex'), QTableWidgetItem(arbitration_id_string))
-                self.tableWidgetMessages.setItem(i, labels.index('ID Dec'),
+                self.tableWidgetMessages.setItem(i, self.labels.index('ID Hex'),
+                                                 QTableWidgetItem(arbitration_id_string))
+                self.tableWidgetMessages.setItem(i, self.labels.index('ID Dec'),
                                                  QTableWidgetItem(str(message.arbitration_id)))
                 data_strings = []
                 if message.data is not None:
                     for index in range(0, min(message.dlc, len(message.data))):
                         data_strings.append(f"{message.data[index]:02x}")
-                self.tableWidgetMessages.setItem(i, labels.index('Data'), QTableWidgetItem(' '.join(data_strings)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('Data'), QTableWidgetItem(' '.join(data_strings)))
                 value_1_u = int.from_bytes(message.data[0:2], byteorder="big", signed=False)
                 value_1_s = int.from_bytes(message.data[0:2], byteorder="big", signed=True)
                 value_2_u = int.from_bytes(message.data[2:4], byteorder="big", signed=False)
@@ -175,30 +316,31 @@ class MainWindow(Ui_MainWindow):
                 value_3_s = int.from_bytes(message.data[4:6], byteorder="big", signed=True)
                 value_4_u = int.from_bytes(message.data[6:8], byteorder="big", signed=False)
                 value_4_s = int.from_bytes(message.data[6:8], byteorder="big", signed=True)
-                self.tableWidgetMessages.setItem(i, labels.index('0U16'), QTableWidgetItem(str(value_1_u)))
-                self.tableWidgetMessages.setItem(i, labels.index('0S16'), QTableWidgetItem(str(value_1_s)))
-                self.tableWidgetMessages.setItem(i, labels.index('1U16'), QTableWidgetItem(str(value_2_u)))
-                self.tableWidgetMessages.setItem(i, labels.index('1S16'), QTableWidgetItem(str(value_2_s)))
-                self.tableWidgetMessages.setItem(i, labels.index('2U16'), QTableWidgetItem(str(value_3_u)))
-                self.tableWidgetMessages.setItem(i, labels.index('2S16'), QTableWidgetItem(str(value_3_s)))
-                self.tableWidgetMessages.setItem(i, labels.index('3U16'), QTableWidgetItem(str(value_4_u)))
-                self.tableWidgetMessages.setItem(i, labels.index('3S16'), QTableWidgetItem(str(value_4_s)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('0U16'), QTableWidgetItem(str(value_1_u)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('0S16'), QTableWidgetItem(str(value_1_s)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('1U16'), QTableWidgetItem(str(value_2_u)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('1S16'), QTableWidgetItem(str(value_2_s)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('2U16'), QTableWidgetItem(str(value_3_u)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('2S16'), QTableWidgetItem(str(value_3_s)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('3U16'), QTableWidgetItem(str(value_4_u)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('3S16'), QTableWidgetItem(str(value_4_s)))
 
                 value_1_u = int.from_bytes(message.data[0:4], byteorder="big", signed=False)
                 value_1_s = int.from_bytes(message.data[0:4], byteorder="big", signed=True)
                 value_2_u = int.from_bytes(message.data[4:8], byteorder="big", signed=False)
                 value_2_s = int.from_bytes(message.data[4:8], byteorder="big", signed=True)
-                self.tableWidgetMessages.setItem(i, labels.index('0U32'), QTableWidgetItem(str(value_1_u)))
-                self.tableWidgetMessages.setItem(i, labels.index('0S32'), QTableWidgetItem(str(value_1_s)))
-                self.tableWidgetMessages.setItem(i, labels.index('1U32'), QTableWidgetItem(str(value_2_u)))
-                self.tableWidgetMessages.setItem(i, labels.index('1S32'), QTableWidgetItem(str(value_2_s)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('0U32'), QTableWidgetItem(str(value_1_u)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('0S32'), QTableWidgetItem(str(value_1_s)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('1U32'), QTableWidgetItem(str(value_2_u)))
+                self.tableWidgetMessages.setItem(i, self.labels.index('1S32'), QTableWidgetItem(str(value_2_s)))
 
                 if message.arbitration_id in self.can_logger.message_interval:
                     interval = self.can_logger.message_interval[message.arbitration_id]
                 else:
                     interval = -1
-                self.tableWidgetMessages.setItem(i, labels.index('Interval'), QTableWidgetItem(f'{interval:.0f}'))
-                self.tableWidgetMessages.setItem(i, labels.index('Channel'), QTableWidgetItem(f'{message.channel}'))
+                self.tableWidgetMessages.setItem(i, self.labels.index('Interval'), QTableWidgetItem(f'{interval:.0f}'))
+                self.tableWidgetMessages.setItem(i, self.labels.index('Channel'),
+                                                 QTableWidgetItem(f'{message.channel}'))
 
 
 def test_dump(filename: str):
