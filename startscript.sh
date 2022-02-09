fac1620448370c0c9012baf445c1a059d7af0ae9
Jesse Kleemann <j.kleemann@tu-braunschweig.de>
2022-02-09 02:35:28 +0100

    Remove startscript. Executing from container does not work

--- startscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/usr/sbin/ip link set can0 type can restart-ms 100
-/usr/sbin/ip link set can1 type can restart-ms 100
-/usr/sbin/ip link set can0 up type can bitrate 500000
-/usr/sbin/ip link set can1 up type can bitrate 500000
-/usr/sbin/ifconfig can0 txqueuelen 100
-/usr/sbin/ifconfig can1 txqueuelen 100
